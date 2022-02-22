a6bbe8ef30737aa8afc3f74bb0543627c5bde374
Anthony Yoskovich <anthony.yoskovich@gmail.com>
2022-02-21 21:04:14 -0500

    Testing title

--- app/ui.R
+++ app/ui.R
@@ -13,7 +13,7 @@ library(shiny)
 shinyUI(fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("The github action is working, I think..."),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
